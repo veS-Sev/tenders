4a1bdafc334276faba0966d5f905bf0b25e5fd43
ves-sev <newsecretword@yandex.ru>
2023-05-11 23:38:38 +0300

    added logic for changing the activeTimerParticipant depending on the  tradingParticipants array change

--- src/app/components/timer/timer.tsx
+++ src/app/components/timer/timer.tsx
@@ -1,15 +1,16 @@
 import "./timer.scss";
+import {memo} from 'react';
 import { useTimer } from "./hook/use-timer.hook";
 import { BsHourglassSplit } from "react-icons/bs";
-import {TStartOfTrading} from '../../features/tradings/types'
+import {TStartOfTrading, TTradingParticipant} from '../../features/tradings/types'
 
+const BsHourglass=memo(BsHourglassSplit);
 
 export type TTimerProp={
-  startOfTrading:TStartOfTrading
+  tradingParticipants:TTradingParticipant[]
 }
 
 export const Timer = (
-  // {startOfTrading}:TTimerProp
   ) => {
   const { secRemaiming, minRemaiming, hourRemaiming} =
   useTimer();
@@ -19,7 +20,7 @@ export const Timer = (
       <div id="hourRemaiming">{String(hourRemaiming).padStart(2, "0")}:</div>
       <div id="minRemaiming">{String(minRemaiming).padStart(2, "0")}:</div>
       <div id="secRemaiming">{String(secRemaiming).padStart(2, "0")}</div>
-      <BsHourglassSplit />
+      <BsHourglass />
     </div>
   );
 };
