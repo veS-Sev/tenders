3a1567d90400eae2f2f89304de6e2adce6d2bbb0
ves-sev <newsecretword@yandex.ru>
2023-06-12 04:05:43 +0300

    Render logic for table cells has moved to TableTd component

--- src/app/features/tender-table/table-body/table-body.tsx
+++ src/app/features/tender-table/table-body/table-body.tsx
@@ -1,59 +1,48 @@
 import "./table-body.scss";
 import { useAppSelector } from "../../../hooks";
 import { TTenderParameters } from "../../../features/tenders/types/index";
-
+import { TTenderParticipant } from "../../tenders/types";
+import { TableTd } from "../table-td/table-td";
 export const TableBody = () => {
   const tender: any = useAppSelector((state) => state.tenderTable.tenderData);
-
-  const activeTimerParticipant = useAppSelector(
-    (state) => state.activeTimerParticipant.id
+  const tenderParticipants = tender.tenderParticipants;
+  const tenderParametrsName = Object.getOwnPropertyNames(
+    tender.tenderParamerts
   );
 
-  const tenderParticipants = tender.tenderParticipants;
+  // Можно вывести в отдельный компонент Forms
+  let forms = tenderParticipants.map((participant: any) => (
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      key={participant.id}
+      id={participant.id}
+    ></form>
+  ));
 
   return (
     <>
+      {forms}
       <tbody>
-        {Object.getOwnPropertyNames(tender.tenderParamerts).map(
-          (parametr: string) => (
-            <tr key={parametr}>
-              <th key={parametr}>
-                {tender.tenderParamerts[parametr as keyof TTenderParameters]}
-              </th>
-              {tenderParticipants.map(
-                //должен быть установлен тип
-                (participant: any, index: number) => (
-                  <td
-                    className={
-                      activeTimerParticipant === participant.id
-                        ? "active participant-column"
-                        : "participant-column"
-                    }
-                    key={participant.id}
-                  >
-                    {(parametr as keyof TTenderParameters) === "name" ? (
-                      <>
-                        <span className={"number-in-order"}>
-                          Участник{index + 1}
-                        </span>
-                        <span className={"participant-name"}>
-                          {participant[parametr as keyof TTenderParameters]}
-                        </span>
-                      </>
-                    ) : (
-                      participant[parametr as keyof TTenderParameters]
-                    )}
-                    {(parametr as keyof TTenderParameters) === "actions" && (
-                      <>
-                        <button disabled={activeTimerParticipant != participant.id}>Сделать ход</button>
-                      </>
-                    )}
-                  </td>
-                )
-              )}
-            </tr>
-          )
-        )}
+        {tenderParametrsName.map((parametr: string) => (
+          <tr key={parametr}>
+            <th key={parametr}>
+              {tender.tenderParamerts[parametr as keyof TTenderParameters]}
+            </th>
+            {tenderParticipants.map(
+              (participant: TTenderParticipant, index: number) => (
+
+                <TableTd
+                  key={participant.id}
+                  participant={participant}
+                  currentParametr={parametr}
+                  index={index}
+                  name={"name"}
+                  actions={"actions"}
+                />
+              )
+            )}
+          </tr>
+        ))}
       </tbody>
     </>
   );
