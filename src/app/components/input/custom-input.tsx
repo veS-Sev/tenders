9fbff0837a6f0a24cc3f862d2b9358063de82dbc
ves-sev <newsecretword@yandex.ru>
2023-12-12 09:56:35 +0300

    the offers are sent if they have been changed,if not, the data of the last offer is displayed,the principle of blocking the button for sending the offer has been changed

--- src/app/components/input/custom-input.tsx
+++ src/app/components/input/custom-input.tsx
@@ -1,11 +1,24 @@
-import{useState,memo}from 'react';
-import {TCustomInput} from './types/custom-input.type'
+import{useState,memo, useEffect}from 'react';
+import {TCustomInput} from './types/custom-input.type';
+import {useAppDispatch } from "../../hooks";
+import {offerIsChanging}from "../../features/tenders-table/store/offer-change.slice"
 
 export const CustomInput=memo(function CustomInput({form,
     disabled,name,startValue}:TCustomInput){
 const[value,setValue]=useState(startValue);
+const dispatch=useAppDispatch();
+useEffect(()=>{
+if(Boolean(disabled)===true){
+    setValue(startValue)
+}},[disabled])
+
+useEffect(()=>{
+    if(value!==startValue){
+        dispatch(offerIsChanging({hasChanged:true,offerForm:form}))}
+},[value])
 const handleInput=(e: React.ChangeEvent<HTMLInputElement>)=>{
     setValue(e.target.value.trim())
+
 }
 const cancellSending=(e: React.KeyboardEvent<HTMLInputElement>)=>e.key=="Enter"&&e.preventDefault()
     return(
--- src/app/features/tenders-table/table-forms/table-forms.tsx
+++ src/app/features/tenders-table/table-forms/table-forms.tsx
@@ -1,12 +1,14 @@
 import { memo } from "react";
 import { useParams } from "react-router-dom";
+import { useAppDispatch } from "../../../hooks";
 import {
   useMakeOfferMutation
 } from "../api/tender.api";
 import { useTendersParticipantsList } from "../hooks/use-tender-participants-list.hook";
-
+import { offerIsChanging } from "../store/offer-change.slice";
 
 export const TableForms = memo(function Forms() {
+  const dispatch=useAppDispatch();
   const { id: tenderId } = useParams();
   //получаю список участников торга
   const tenderParticipants: any[] = useTendersParticipantsList(tenderId);
@@ -18,6 +20,7 @@ export const TableForms = memo(function Forms() {
     participantId: string
   ) => {
     e.preventDefault();
+    dispatch(offerIsChanging({hasChanged:false}))
     console.log("событие формы");
     const form = e.currentTarget;
 
